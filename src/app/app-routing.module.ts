f571deb9c4db70f0e568b9c5e66f7a5b2fe16a15
VietDong2904 <dovietdong2904@gmail.com>
2023-05-26 13:45:26 +0700

    fontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassifyComponent } from './classify/classify.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './service/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'classify', component: ClassifyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  // providers: [AuthGuard]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {TabViewModule} from 'primeng/tabview';
+import {ButtonModule} from 'primeng/button';
+import { LoginComponent } from './login/login.component';
+import {CheckboxModule} from 'primeng/checkbox';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {DividerModule} from 'primeng/divider';
+import {CalendarModule} from 'primeng/calendar';
+import { MessageService } from 'primeng/api';
+import {InputTextModule} from 'primeng/inputtext';
+import { ClassifyComponent } from './classify/classify.component';
+import {FileUploadModule} from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {InplaceModule} from 'primeng/inplace';
+import { UserUpdateComponent } from './user-update/user-update.component';
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClassifyComponent,
+    UserUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    TabViewModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    DividerModule,
+    CalendarModule,
+    FileUploadModule,
+    HttpClientModule,
+    TableModule,
+    ToastModule,
+    BrowserAnimationsModule,
+    InplaceModule,
+    DynamicDialogModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
